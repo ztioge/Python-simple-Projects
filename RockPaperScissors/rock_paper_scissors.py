bb04fc0049aac694914d67cc2ab0a6349db916c4
Egoitz <egoitzu@telefonica.net>
2023-04-25 21:47:21 +0200

    Basic Rock, Paper, Scissors game
    Basic Rock, Paper, Scissors game that the user plays through the terminal.

---
+++ RockPaperScissors/rock_paper_scissors.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user = input("Whats your choice? 'r' for rock, 'p' for paper, 's' for scissors \n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'Its a tie'
+    
+    if is_win(user, computer):
+        return 'You won!'
+    else:
+        return 'You lost!'
+
+def is_win(player, opponent):
+
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+       return True
+    
+
+print(play())
